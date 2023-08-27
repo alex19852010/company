61340e93848eaa1aca542e631e999d0bb8b890c2
Alex Bogushenko <bogushenkoas@ukr.net>
2023-08-27 14:24:47 +0300

    mycommit new

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-class Worker {
+ class Worker {
 public:
     std::string name;
 
@@ -18,105 +18,70 @@ class Manager : public Worker {
 public:
     explicit Manager(const std::string& name) : Worker(name) {}
 
-    void assignTasks(int commandId) {
+    void assignTasks(int commandId, std::vector<Worker*>& workers) {
         int seed = commandId + this->name.length();
         std::srand(seed);
 
-        int tasksCount = rand() % (team->workers.size() + 1);
+        int tasksCount = rand() % (workers.size() + 1);
         std::cout << "Manager " << this->name << " assigned " << tasksCount << " tasks to the team." << std::endl;
 
+        std::vector<char> taskTypes = {'A', 'B', 'C'};
         for (int i = 0; i < tasksCount; i++) {
-            int workerIndex = rand() % team->workers.size();
-            Worker* worker = team->workers[workerIndex];
-            team->assignTask(worker);
+            int workerIndex = rand() % workers.size();
+            Worker* worker = workers[workerIndex];
+            char taskType = taskTypes[rand() % taskTypes.size()];
+            assignTask(worker, taskType);
         }
     }
 
-    void setTeam(std::vector<Worker*>& workers) {
-        team = new Team(workers);
+    void assignTask(Worker* worker, char taskType) {
+        std::cout << "Worker " << worker->name << " received a task of type " << taskType << "." << std::endl;
     }
-
-private:
-    class Team {
-    public:
-        std::vector<Worker*> workers;
-
-        explicit Team(std::vector<Worker*>& workers) : workers(workers) {}
-
-        void assignTask(Worker* worker) {
-            std::cout << "Worker " << worker->name << " received a task." << std::endl;
-        }
-    };
-
-    Team* team;
 };
 
 class CEO : public Worker {
 public:
     explicit CEO(const std::string& name) : Worker(name) {}
 
-    void setManagers(std::vector<Manager*>& managers) {
-        this->managers = managers;
-    }
+    void startSimulation(int numCommands, int numWorkers) {
+        std::vector<Manager*> managers;
+        std::vector<int> commands;
 
-    void setCommands(std::vector<int>& commands) {
-        this->commands = commands;
-    }
+        for (int i = 0; i < numCommands; i++) {
+            std::string managerName = "Manager " + std::to_string(i);
+            Manager* manager = new Manager(managerName);
 
-    void startSimulation() {
-        for (int command : commands) {
-            int managerIndex = command % managers.size();
-            Manager* manager = managers[managerIndex];
-            manager->assignTasks(command);
+            std::vector<Worker*> workers;
+            for (int j = 0; j < numWorkers; j++) {
+                std::string workerName = "Worker " + std::to_string(i * numWorkers + j);
+                Worker* worker = new Worker(workerName);
+                workers.push_back(worker);
+            }
+
+            managers.push_back(manager);
+            commands.push_back(i);
+            manager->assignTasks(i, workers);
+        }
+
+        // Освобождаем память
+        for (Manager* manager : managers) {
+            delete manager;
         }
     }
-
-private:
-    std::vector<Manager*> managers;
-    std::vector<int> commands;
 };
 
 int main()
 {
-      int numCommands, numWorkers;
+    int numCommands, numWorkers;
     std::cout << "Enter the number of commands: ";
     std::cin >> numCommands;
     std::cout << "Enter the number of workers per command: ";
     std::cin >> numWorkers;
 
-    std::vector<Manager*> managers;
-    std::vector<int> commands;
-
-    for (int i = 0; i < numCommands; i++) {
-        std::string managerName = "Manager " + std::to_string(i);
-        Manager* manager = new Manager(managerName);
-
-        std::vector<Worker*> workers;
-        for (int j = 0; j < numWorkers; j++) {
-            std::string workerName = "Worker " + std::to_string(i * numWorkers + j);
-            Worker* worker = new Worker(workerName);
-            workers.push_back(worker);
-        }
-
-        manager->setTeam(workers);
-        managers.push_back(manager);
-        commands.push_back(i);
-    }
-
     CEO* ceo = new CEO("CEO");
-    ceo->setManagers(managers);
-    ceo->setCommands(commands);
-    ceo->startSimulation();
+    ceo->startSimulation(numCommands, numWorkers);
 
-    // Освобождаем память
-    for (Manager* manager : managers) {
-        delete manager;
-    }
-    delete ceo;  
-    
-     /* Программа начинается с запроса количества команд и количество работников в каждой команде. 
-Затем создаются менеджеры и рабочие, и им назначаются задачи на основе случайных чисел. В конце освобождается выделенная память.
-
-Обратите внимание, что в этом коде нет обработки ошибок и валидации пользовательского ввода. В реальном проекте это также следует учесть.*/
+    delete ceo;
+ 
     return 0;
 }
