ef845f64bd3489a8df6a01e3973bd34992cde2c9
Alex Bogushenko <bogushenkoas@ukr.net>
2023-08-27 14:36:59 +0300

    2 new commit

--- main.cpp
+++ main.cpp
@@ -9,23 +9,23 @@ using namespace std;
 
  class Worker {
 public:
-    std::string name;
+    string name;
 
-    explicit Worker(const std::string& name) : name(name) {}
+     Worker(const string& name) : name(name) {}
 };
 
 class Manager : public Worker {
 public:
-    explicit Manager(const std::string& name) : Worker(name) {}
+     Manager(const string& name) : Worker(name) {}
 
-    void assignTasks(int commandId, std::vector<Worker*>& workers) {
+    void assignTasks(int commandId, vector<Worker*>& workers) {
         int seed = commandId + this->name.length();
-        std::srand(seed);
+        srand(seed);
 
         int tasksCount = rand() % (workers.size() + 1);
-        std::cout << "Manager " << this->name << " assigned " << tasksCount << " tasks to the team." << std::endl;
+        cout << "Manager " << this->name << " assigned " << tasksCount << " tasks to the team." << endl;
 
-        std::vector<char> taskTypes = {'A', 'B', 'C'};
+        vector<char> taskTypes = {'A', 'B', 'C'};
         for (int i = 0; i < tasksCount; i++) {
             int workerIndex = rand() % workers.size();
             Worker* worker = workers[workerIndex];
@@ -35,25 +35,25 @@ public:
     }
 
     void assignTask(Worker* worker, char taskType) {
-        std::cout << "Worker " << worker->name << " received a task of type " << taskType << "." << std::endl;
+        cout << "Worker " << worker->name << " received a task of type " << taskType << "." << endl;
     }
 };
 
 class CEO : public Worker {
 public:
-    explicit CEO(const std::string& name) : Worker(name) {}
+    explicit CEO(const string& name) : Worker(name) {}
 
     void startSimulation(int numCommands, int numWorkers) {
-        std::vector<Manager*> managers;
-        std::vector<int> commands;
+        vector<Manager*> managers;
+        vector<int> commands;
 
         for (int i = 0; i < numCommands; i++) {
-            std::string managerName = "Manager " + std::to_string(i);
+            string managerName = "Manager " + to_string(i);
             Manager* manager = new Manager(managerName);
 
             std::vector<Worker*> workers;
             for (int j = 0; j < numWorkers; j++) {
-                std::string workerName = "Worker " + std::to_string(i * numWorkers + j);
+                string workerName = "Worker " + to_string(i * numWorkers + j);
                 Worker* worker = new Worker(workerName);
                 workers.push_back(worker);
             }
@@ -73,10 +73,10 @@ public:
 int main()
 {
     int numCommands, numWorkers;
-    std::cout << "Enter the number of commands: ";
-    std::cin >> numCommands;
-    std::cout << "Enter the number of workers per command: ";
-    std::cin >> numWorkers;
+    cout << "Enter the number of commands: ";
+    cin >> numCommands;
+    cout << "Enter the number of workers per command: ";
+    cin >> numWorkers;
 
     CEO* ceo = new CEO("CEO");
     ceo->startSimulation(numCommands, numWorkers);
